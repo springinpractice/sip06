ecc954ce515540d7a743597b812388173abdd6d8
Willie Wheeler <willie.wheeler@gmail.com>
2011-07-19 01:01:51 -0700

    Modified database schema and updated spring security config to match

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,9 @@
-insert into users values ('juan', 'juan', 1);
-insert into users values ('elvira', 'elvira', 1);
+insert into account values (1, 'juan', 'Juan', 'Cazares', 'juan.cazares@example.com', 'juan', 1, 1, 1, null, null);
+insert into account values (2, 'elvira', 'Elvira', 'Cazares', 'elvira.cazares@example.com', 'elvira', 1, 1, 1, null, null);
 
-insert into authorities values ('juan', 'user');
-insert into authorities values ('juan', 'admin');
-insert into authorities values ('elvira', 'user');
+insert into role values (1, 'user');
+insert into role values (2, 'admin');
+
+insert into account_role values (0, 1, 1);
+insert into account_role values (0, 1, 2);
+insert into account_role values (0, 2, 1);
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,15 +1,33 @@
-drop table if exists authorities;
-drop table if exists users;
+drop table if exists account_role;
+drop table if exists role;
+drop table if exists account;
 
-create table users (
-    username varchar(50) not null primary key,
-    password varchar(50) not null,
-    enabled boolean not null
+create table account (
+    id bigint unsigned not null auto_increment primary key,
+    username varchar(50) unique not null,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(50) not null,
+    password varchar(50),
+    marketing_ok boolean not null,
+    accept_terms boolean not null,
+    enabled boolean not null,
+    date_created timestamp default 0,
+    date_modified timestamp default current_timestamp on update current_timestamp,
+    unique index account_idx_1 (username),
+    unique index account_idx_2 (email)
 ) engine = InnoDb;
 
-create table authorities (
-    username varchar(50) not null,
-    authority varchar(50) not null,
-    foreign key (username) references users (username),
-    unique index authorities_idx_1 (username, authority)
+create table role (
+    id bigint unsigned not null auto_increment primary key,
+    name varchar(50) not null unique
+) engine = InnoDb;
+
+create table account_role (
+    id bigint unsigned not null auto_increment primary key,
+    account_id bigint unsigned not null,
+    role_id bigint unsigned not null,
+    foreign key (account_id) references account (id),
+    foreign key (role_id) references role(id),
+    unique index account_role_idx_1 (account_id, role_id)
 ) engine = InnoDb;
