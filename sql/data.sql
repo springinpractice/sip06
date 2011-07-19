9d909246ba4da53d34db114c11c0c20e3d846996
Willie Wheeler <willie.wheeler@gmail.com>
2011-07-19 00:39:11 -0700

    Added some stuff I forgot to add

---
+++ sql/data.sql
@@ -0,0 +1,6 @@
+insert into users values ('juan', 'juan', 1);
+insert into users values ('elvira', 'elvira', 1);
+
+insert into authorities values ('juan', 'user');
+insert into authorities values ('juan', 'admin');
+insert into authorities values ('elvira', 'user');
---
+++ sql/schema.sql
@@ -0,0 +1,15 @@
+drop table if exists authorities;
+drop table if exists users;
+
+create table users (
+    username varchar(50) not null primary key,
+    password varchar(50) not null,
+    enabled boolean not null
+) engine = InnoDb;
+
+create table authorities (
+    username varchar(50) not null,
+    authority varchar(50) not null,
+    foreign key (username) references users (username),
+    unique index authorities_idx_1 (username, authority)
+) engine = InnoDb;
